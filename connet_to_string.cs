d1b2bc06f15bdd867f7eec88970a9294de4c7068
SADMAN SHAKIB AKIB <68108387+Akib116@users.noreply.github.com>
2020-07-13 23:39:18 +0600

    Readme

---
+++ connet_to_string.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_two_string
+{
+    class connet_to_string
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First name=");
+            String Firstname = Console.ReadLine();
+            Console.WriteLine("Enter Second name=");
+            String Secondname = Console.ReadLine();
+            String Fullname = Firstname + Secondname;
+            Console.WriteLine("Full name is={0}", Fullname);
+            Console.ReadKey();
+        }
+    }
+}
