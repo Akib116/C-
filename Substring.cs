3960277bef1a3cafc67d85e0467a389693511f1c
SADMAN SHAKIB AKIB <68108387+Akib116@users.noreply.github.com>
2020-07-14 21:29:27 +0600

    Readme

---
+++ Substring.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substring
+{
+    class Substring
+    {
+        static void Main(string[] args)
+        {
+            string name = "The dog is barking";
+            Console.WriteLine(name.Substring(0, 7));
+
+            Console.ReadKey();
+        }
+    }
+}
