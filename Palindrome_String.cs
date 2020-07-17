428bc2b747d9c1f46a4a1a80b2c637e3e677dc1e
SADMAN SHAKIB AKIB <68108387+Akib116@users.noreply.github.com>
2020-07-17 22:31:29 +0600

    Readme

---
+++ Palindrome_String.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_String
+{
+    class Palindrome_String
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide input string");
+            string input = Console.ReadLine();
+            string reverse = string.Empty;
+            int length = input.Length - 1;
+
+            while(length>=0)
+            {
+                reverse = reverse + input[length];
+                length--;
+            }
+            if (input == reverse)
+            {
+                Console.WriteLine($"{input} is palindrome string");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is not palindrome string");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Palindrome_number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palindrome_number
+{
+    class Palindrome_number
+    {
+        static void Main(string[] args)
+        {
+            int num, temp, revNum = 0, rem;
+            Console.WriteLine("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            temp = num;
+
+            while(num>0)
+            {
+                rem = num % 10;
+                revNum = revNum * 10 + rem;
+                num = num / 10;
+            }
+            if(revNum==temp)
+            {
+                Console.WriteLine("Number is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Number is not palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
