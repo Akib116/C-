c4ef8405752a815b0bc1a71fa37d2a0fffb19452
SADMAN SHAKIB AKIB <68108387+Akib116@users.noreply.github.com>
2020-07-19 17:33:20 +0600

    Readme

---
+++ Constructor/Constructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Constructor
+{
+    class Constructor
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("SADMAN", "SHAKIB", "AKIB");
+
+            string fullname = p1.GetFullName();
+            Console.WriteLine(fullname);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Constructor/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Constructor
+{
+    class Person
+    {
+        public Person(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+       public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + MiddleName + " " + LastName;
+        }
+
+      
+    }
+    
+}
