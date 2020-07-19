4aa09305ab38044fb2d95dbeab39394145512881
SADMAN SHAKIB AKIB <68108387+Akib116@users.noreply.github.com>
2020-07-19 21:35:56 +0600

    Readme

---
+++ Inheritance/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+   public class Account
+    {
+        public string AccountNo { get; set; }
+        public string OpeningDate { get; set; }
+        public double Balance { get; private set; }
+
+
+        public bool Deposit(double amount)
+        {
+            Balance += amount;
+            return true;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            Balance -= amount;
+            return true;
+        }
+
+    }
+}
---
+++ Inheritance/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inheritance
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            SavingAccount s1 = new SavingAccount();
+            s1.AccountNo = "001";
+            s1.OpeningDate = "19/07/2020";
+            s1.InterestAmount = 100;
+            s1.Deposit(500);
+            s1.Withdraw(200);
+
+            Console.WriteLine(s1.AccountNo);
+            Console.WriteLine(s1.OpeningDate);
+            Console.WriteLine(s1.InterestAmount);
+            Console.WriteLine(s1.Balance);
+            Console.ReadKey();
+
+
+        }
+    }
+}
