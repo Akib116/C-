412092d04431a267a58f3b26fa1eb190175548b7
SADMAN SHAKIB AKIB <68108387+Akib116@users.noreply.github.com>
2020-08-05 18:50:07 +0600

    Readme

---
+++ Fibonacci Series.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max, add = 1, i = 0, j = 1;
+            Console.Write("Please enter Limit :");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.Write(i);
+
+            while(add<=max)
+            {
+                Console.Write(" "+ add +" ");
+                add = i + j;
+                i = j;
+                j = add;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinearSearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linear_Search
+{
+    class LinearSearch
+    {
+        static void Main(string[] args)
+        {
+            int[] value = { 1, 3, 5, 6, 9, 7, 11, 13, 17, 21, 18, 15, 20 };
+            Console.WriteLine("Enter seach number: ");
+            int givenNumber = Convert.ToInt32(Console.ReadLine());
+            bool searchFound = false;
+
+            for(int i=0;i<value.Length; i++)
+            {
+                if (value[i] == givenNumber) {
+                Console.WriteLine("Search is Successful");
+                Console.WriteLine($"The given number {givenNumber} is in {i} index or {i+1} position");
+                    searchFound = true;
+                }
+            }
+            if (!searchFound)
+                Console.WriteLine("Search is unsuccessful");
+            Console.ReadKey();
+        }
+
+    }
+}
